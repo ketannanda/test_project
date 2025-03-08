b4f0fa9404499cc56514f422d0dfe341a7c8b4c3
ASUS <ketannandanwar97@gmail.com>
2025-03-08 15:07:45 +0530

    First Commit

---
+++ src/main/java/com/psa/PsaGitApplication.java
@@ -0,0 +1,13 @@
+package com.psa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsaGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsaGitApplication.class, args);
+	}
+
+}
